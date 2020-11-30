a3e907ab144e78cca78ce61a80e512e954f8c4ef
Sidharrth <sidharrthsahai9@gmail.com>
2020-11-30 11:12:07 +0000

    making classes shop and product

---
+++ product.rb
@@ -0,0 +1,14 @@
+class Product
+  def initialize(name, net_price, count)
+    @name, @net_price, @count = name, net_price, count
+  end
+
+  def net_price
+
+  end
+
+  def gross_price
+
+  end
+
+end
