9df492aa3770224be4878de55c1ad1715701bbf5
Sidharrth <sidharrthsahai9@gmail.com>
2020-11-30 11:38:42 +0000

    writing shop class

--- shop.rb
+++ shop.rb
@@ -1,3 +1,20 @@
-class Shop
 
+
+class Shop
+  attr_reader :products
+
+  def initialize
+    @products = []
+  end
+
+  def add_product(prod)
+    @products << prod
+  end
+
+  def to_s
+    str = "Products in Shop: \n"
+    @products.each do |prod|
+      str += prod.to_s + "\n"
+    end
+  end
 end
